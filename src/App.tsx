aca7ee273d484b204dc40122bb1399998483777e
Hornflakes <cristianpetrugug@gmail.com>
2024-02-14 19:49:35 +0200

    feat: tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ const App: Component = () => {
                     Edit <code>src/App.tsx</code> and save to reload.
                 </p>
                 <a
-                    class={styles.link}
+                    class="text-4xl text-blue-600"
                     href="https://github.com/solidjs/solid"
                     target="_blank"
                     rel="noopener noreferrer"
