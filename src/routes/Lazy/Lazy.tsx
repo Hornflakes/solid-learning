6901c80d23305583f7217cd29a15d532aaf06af1
Cristi <36103619+Hornflakes@users.noreply.github.com>
2024-02-17 02:29:12 +0200

    chore: delete uppercase lazy route

--- src/routes/Lazy/Lazy.tsx
+++
@@ -1,7 +0,0 @@
-import { Component } from 'solid-js';
-
-const LazyPage: Component = () => {
-    return <p class="flex flex-grow items-center justify-center text-2xl">i'm lazy</p>;
-};
-
-export default LazyPage;
